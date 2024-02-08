70c2b01e9e0ed78f439f7ed7e370356aeb5a7163
Julyfan77 <630249154@qq.com>
2024-02-08 20:28:25 +0800

    roc

---
+++ roc/app.js
@@ -0,0 +1,202 @@
+
+$(document).ready(function() {
+    var editor = CodeMirror.fromTextArea(document.getElementById("bibtex"), {
+        lineNumbers: false,
+        lineWrapping: true,
+        readOnly:true
+    });
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+    
+
+    // Listening to the video element
+    let vid1 = document.querySelector(".vid1")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid1.addEventListener("mouseover", function (e) {
+        vid1.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid1.addEventListener("mouseout", function (e) {
+        vid1.muted = true;
+    })
+
+    // Listening to the video element
+    let vid2 = document.querySelector(".vid2")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid2.addEventListener("mouseover", function (e) {
+        vid2.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid2.addEventListener("mouseout", function (e) {
+        vid2.muted = true;
+    })
+
+    // Listening to the video element
+    let vid3 = document.querySelector(".vid3")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid3.addEventListener("mouseover", function (e) {
+        vid3.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid3.addEventListener("mouseout", function (e) {
+        vid3.muted = true;
+    })
+
+    // Listening to the video element
+    let vid4 = document.querySelector(".vid4")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid4.addEventListener("mouseover", function (e) {
+        vid4.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid4.addEventListener("mouseout", function (e) {
+        vid4.muted = true;
+    })
+
+    // Listening to the video element
+    let vid5 = document.querySelector(".vid5")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid5.addEventListener("mouseover", function (e) {
+        vid5.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid5.addEventListener("mouseout", function (e) {
+        vid5.muted = true;
+    })
+
+    // Listening to the video element
+    let vid6 = document.querySelector(".vid6")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid6.addEventListener("mouseover", function (e) {
+        vid6.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid6.addEventListener("mouseout", function (e) {
+        vid6.muted = true;
+    })
+
+    // Listening to the video element
+    let vid7 = document.querySelector(".vid7")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid7.addEventListener("mouseover", function (e) {
+        vid7.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid7.addEventListener("mouseout", function (e) {
+        vid7.muted = true;
+    })
+
+    // Listening to the video element
+    let vid8 = document.querySelector(".vid8")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid8.addEventListener("mouseover", function (e) {
+        vid8.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid8.addEventListener("mouseout", function (e) {
+        vid8.muted = true;
+    })
+
+    // Listening to the video element
+    let vid9 = document.querySelector(".vid9")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid9.addEventListener("mouseover", function (e) {
+        vid9.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid9.addEventListener("mouseout", function (e) {
+        vid9.muted = true;
+    })
+
+    // Listening to the video element
+    let vid10 = document.querySelector(".vid10")
+
+    /* Adding the event listeners on the video to play/pause the video. */
+    vid10.addEventListener("mouseover", function (e) {
+        vid10.muted = false;
+    })
+
+    /* Applying the mouse out event to pause the video */
+    vid10.addEventListener("mouseout", function (e) {
+        vid10.muted = true;
+    })
+});
+
+
+
+// function toggleMute(el) {
+//     el.muted = !(el.muted);
+//   }
+  
+// window.onload = function() {
+//     document.getElementById("sos1").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos1").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
+
+// window.onload = function() {
+//     document.getElementById("sos2").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos2").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
+
+// window.onload = function() {
+//     document.getElementById("sos3").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos3").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
+
+// window.onload = function() {
+//     document.getElementById("sos4").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos4").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
+
+// window.onload = function() {
+//     document.getElementById("sos5").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos5").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
+
+// window.onload = function() {
+//     document.getElementById("sos6").onmouseover = function() {
+//         toggleMute(this);
+//     };
+//     document.getElementById("sos6").onmouseout = function() {
+//         toggleMute(this)
+//     };
+// }
